1795a4e6156833fba4fd9d60f86a2bbf75710f3a
theft <gentledemeiz@gmail.com>
2023-10-22 02:45:55 +0300

    правки test

--- src/test/java/ru/practicum/shareit/ShareItTests.java
+++ src/test/java/ru/practicum/shareit/ShareItTests.java
@@ -1,13 +1,11 @@
 package ru.practicum.shareit;
 
-import org.junit.jupiter.api.Test;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
 class ShareItTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
